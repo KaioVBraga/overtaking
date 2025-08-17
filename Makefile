aac0c17cd2349e1a92bbde36c96b71eb67b5e62e
kaiovbraga <kaiovbraga2001@gmail.com>
2025-08-17 19:54:16 -0300

    test: Gemini test 4

--- Makefile
+++ Makefile
@@ -7,14 +7,12 @@ BOT_LIB = py_bot.so
 CXX = g++
 
 # --- Direct paths to TORCS and Python installations ---
-# Add -I/usr/local/include to find sub-libraries like tmath
 TORCS_CFLAGS = -I/usr/local/include/torcs -I/usr/local/include
 TORCS_LDFLAGS = -L/usr/local/lib -ltorcs-robot
 PYTHON_CFLAGS = `python3-config --cflags`
 PYTHON_LDFLAGS = `python3-config --ldflags --embed`
 
 # --- Combined flags ---
-# Add -std=c++11 to enable modern C++ features like 'override'
 CXXFLAGS = -fPIC -shared -std=c++11 $(TORCS_CFLAGS) $(PYTHON_CFLAGS)
 LDFLAGS = $(TORCS_LDFLAGS) $(PYTHON_LDFLAGS)
 
--- py_wrapper.cpp
+++ py_wrapper.cpp
@@ -1,84 +1,95 @@
-#include "py_wrapper.h"
-
 #include <Python.h>
+#include <robot.h>
 
 #include <iostream>
 
 // --- Global Python Objects ---
-PyObject *pModule, *pFunc_drive;
+static PyObject *pModule, *pFunc_drive;
 
-// --- Bot Functions ---
+// --- C-style functions that will be our bot's callbacks ---
 
-// Called once at the start of the race
-void PyBot::init(int *index) {
-  // Initialize the Python Interpreter
+// This is the main driving function called by TORCS every simulation step
+static void pyDrive(int index, tCarElt *car, tSituation *s) {
+  // 1. Create a Python dictionary from the car's state (tCarElt)
+  PyObject *pStateDict = PyDict_New();
+  PyDict_SetItemString(pStateDict, "speed_x", PyFloat_FromDouble(car->pub.speed.x));
+  PyDict_SetItemString(pStateDict, "angle", PyFloat_FromDouble(car->pub.angle));
+  PyDict_SetItemString(pStateDict, "track_pos", PyFloat_FromDouble(car->pub.trkPos.toMiddle));
+
+  // Add track edge sensors
+  PyObject *pTrackSensors = PyList_New(19);
+  for (int i = 0; i < 19; ++i) {
+    PyList_SetItem(pTrackSensors, i, PyFloat_FromDouble(car->pub.sensors.track[i]));
+  }
+  PyDict_SetItemString(pStateDict, "track_sensors", pTrackSensors);
+
+  // 2. Call the Python 'drive' function
+  PyObject *pArgs = PyTuple_Pack(1, pStateDict);
+  PyObject *pResult = PyObject_CallObject(pFunc_drive, pArgs);
+  Py_DECREF(pStateDict);
+  Py_DECREF(pArgs);
+
+  // 3. Parse the returned dictionary and apply commands to the car
+  if (pResult != NULL) {
+    car->ctrl.accelCmd = PyFloat_AsDouble(PyDict_GetItemString(pResult, "accel"));
+    car->ctrl.brakeCmd = PyFloat_AsDouble(PyDict_GetItemString(pResult, "brake"));
+    car->ctrl.steer = PyFloat_AsDouble(PyDict_GetItemString(pResult, "steer"));
+    Py_DECREF(pResult);
+  } else {
+    PyErr_Print();
+    std::cerr << "Error: Python 'drive' function call failed." << std::endl;
+  }
+
+  // Set other commands for simplicity
+  car->ctrl.gear = 1;
+  car->ctrl.clutchCmd = 0;
+}
+
+// Called once for each new race
+static void pyNewRace(int index, tCarElt *car, tSituation *s) {
   Py_Initialize();
-
-  // Add the current directory to Python's path to find our script
   PyObject *sys = PyImport_ImportModule("sys");
   PyObject *path = PyObject_GetAttrString(sys, "path");
   PyList_Append(path, PyUnicode_FromString("."));
 
-  // Load our Python script as a module
   pModule = PyImport_ImportModule("my_bot");
   if (!pModule) {
     PyErr_Print();
-    std::cerr << "Error: Could not import python module 'my_bot.py'.\n";
+    std::cerr << "Error: Could not import python module 'my_bot.py'." << std::endl;
     return;
   }
 
-  // Get a reference to the drive function
   pFunc_drive = PyObject_GetAttrString(pModule, "drive");
   if (!pFunc_drive || !PyCallable_Check(pFunc_drive)) {
     PyErr_Print();
-    std::cerr << "Error: Could not find function 'drive' in 'my_bot.py'.\n";
+    std::cerr << "Error: Could not find function 'drive' in 'my_bot.py'." << std::endl;
     return;
   }
 }
 
-// Called for every simulation step
-CarControl PyBot::drive(CarState &cs) {
-  CarControl cc;  // The command to return
-
-  // 1. Create a Python dictionary from the CarState object
-  PyObject *pStateDict = PyDict_New();
-  PyDict_SetItemString(pStateDict, "speed_x", PyFloat_FromDouble(cs.getSpeedX()));
-  PyDict_SetItemString(pStateDict, "angle", PyFloat_FromDouble(cs.getAngle()));
-  PyDict_SetItemString(pStateDict, "track_pos", PyFloat_FromDouble(cs.getTrackPos()));
-  // Add track edge sensors
-  PyObject *pTrackSensors = PyList_New(19);
-  for (int i = 0; i < 19; ++i) {
-    PyList_SetItem(pTrackSensors, i, PyFloat_FromDouble(cs.getTrack(i)));
-  }
-  PyDict_SetItemString(pStateDict, "track_sensors", pTrackSensors);
-
-  // 2. Call the Python 'drive' function with the state dictionary
-  PyObject *pArgs = PyTuple_Pack(1, pStateDict);
-  PyObject *pResult = PyObject_CallObject(pFunc_drive, pArgs);
-
-  Py_DECREF(pStateDict);
-  Py_DECREF(pArgs);
-
-  // 3. Parse the returned dictionary into our CarControl object
-  if (pResult != NULL) {
-    cc.setAccel(PyFloat_AsDouble(PyDict_GetItemString(pResult, "accel")));
-    cc.setBrake(PyFloat_AsDouble(PyDict_GetItemString(pResult, "brake")));
-    cc.setSteer(PyFloat_AsDouble(PyDict_GetItemString(pResult, "steer")));
-    Py_DECREF(pResult);
-  } else {
-    PyErr_Print();
-    std::cerr << "Error: Python 'drive' function call failed.\n";
-  }
-
-  return cc;
-}
-
-// Called when the race ends
-void PyBot::onShutdown() {
-  // Clean up Python objects
+// Called once when the bot is unloaded
+static void pyShutdown(int index) {
   Py_XDECREF(pFunc_drive);
   Py_XDECREF(pModule);
-
-  // Shutdown the Python Interpreter
   Py_Finalize();
+}
+
+// --- TORCS Interface Setup ---
+
+// This struct holds the function pointers that TORCS will call
+static tRobotItf robotItf =
+    {
+        (tfRbNewTrack)NULL,
+        (tfRbNewRace)pyNewRace,
+        (tfRbEndRace)NULL,
+        (tfRbDrive)pyDrive,
+        (tfRbPitCmd)NULL,
+        (tfRbShutdown)pyShutdown,
+        0  // index
+};
+
+// This is the function that TORCS looks for in the .so file
+extern "C" tRobotItf *robotGetItf(int index) {
+  robotItf.index = index;
+  return &robotItf;
 }
